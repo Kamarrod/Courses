aefceea3fae298be47f95b75f0b36f3c5a193fa0
Kamarrod <127785260+Kamarrod@users.noreply.github.com>
2023-05-27 16:25:03 +0300

    Delete my Api-keys from git

--- Courses.Domain/Api-keys/API_keys.cs
+++ Courses.Domain/Api-keys/API_keys.cs
@@ -8,9 +8,9 @@ namespace Courses.Domain.Api_keys
 {
     public class API_keys
     {
-        public readonly string YouTubeAPI = "AIzaSyAF1XAHnT14ea7UQ5_WeOtQlNvdT8BYI3o";
-        public readonly string OpenAiAPI = "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";
-        public readonly string OAuthClientId = "732438680703-uil3evhrfkq01nob8gv6jkc3ec4ros1j.apps.googleusercontent.com";
-        public readonly string OAuthClientSecret = "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";
+        public readonly string YouTubeAPI = "Api-key";
+        public readonly string OpenAiAPI = "Api-key";
+        public readonly string OAuthClientId = "Api-key";
+        public readonly string OAuthClientSecret = "Api-key";
     }
 }
